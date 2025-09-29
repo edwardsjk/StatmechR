1f265c8fb11af08ecc4b4f65a508d051215dce80
Abagael Sykes (abagaels) <abagaels@c0300ood18.ll.unc.edu>
2025-09-29 18:46:28 -0400

    Changed epiK size to depend on variable x

--- data-raw/generate_vig_data.R
+++ data-raw/generate_vig_data.R
@@ -4,18 +4,18 @@
 #### Vignette 1 ####
 ####################
 
-n <- 30
+n <- 100
 id <- c(1:n)
 x <- ifelse(id %% 2 == 0, 0, 1)
 
-k <- 1000
+k <- ifelse(x == 1, 1000, 750)
 spread <- 10
-peaktime <- 30 + id * 5
+peaktime <- round(30 + id * 1.5)
 tau <- 225
 daily_count <- list()
 dcnts <- list()
 for(i in 1:n){
-  dcnts[[i]] <- round(StatmechR::normmdl(c(k, peaktime[i], log(spread)), tau))
+  dcnts[[i]] <- round(StatmechR::normmdl(c(k[i], peaktime[i], log(spread)), tau))
   daily_count[[i]] <- cbind(rep(id[i], tau), c(1:tau), x[i], dcnts[[i]])
 }
 
@@ -23,5 +23,5 @@ counts <- do.call(rbind, daily_count)
 counts <- as.data.frame(counts)
 names(counts) <- c("location", "time", "variable_1", "cases")
 
-vig1_data <- counts
+vig1_data <- counts %>% mutate(pop_size = 1000)
 save(vig1_data, file = "data/vig1_data.RData")
